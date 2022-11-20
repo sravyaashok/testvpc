ccfdba2b37fb16864ba5f75bae270e6a6bc1447c
sravyaashok <98906904+sravyaashok@users.noreply.github.com>
2022-11-20 18:24:28 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 terraform {
+  backend "s3" {}
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "4.40.0"
-      region  = "us-west-2"
     }
-  }
-  backend "s3" {
   }
 }
